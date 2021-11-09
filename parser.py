feb96ce317bd98a9e6e60c7dc7a94fa053c7f874
Anton M <9062373@gmail.com>
2021-11-09 16:42:01 +0300

    initial commit

---
+++ parser.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+HOST = 'https://www.decathlon.ru/'
+URL = 'https://www.decathlon.ru/C-261832-lyzhi'
+HEADERS = {
+    'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'
+}
+
+
+def get():
+    pass
